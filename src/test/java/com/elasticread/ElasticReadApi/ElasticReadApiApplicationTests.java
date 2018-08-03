f5f509f9297b54b3c0bcd6c14d14cdf39c7413b0
Ravichandran G <ravichandranatusa@gmail.com>
2018-08-02 23:38:24 -0700

    first commit

---
+++ src/test/java/com/elasticread/ElasticReadApi/ElasticReadApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.elasticread.ElasticReadApi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ElasticReadApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
